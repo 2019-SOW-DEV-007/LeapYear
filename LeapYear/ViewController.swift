6787f673222b0ad3b7ba6d0103b341478dfb8592
007 <007@kata.com>
2019-05-09 00:19:28 +0200

    Initial Project Structure Added

--- LeapYear/ViewController.swift
+++ LeapYear/ViewController.swift
@@ -1,20 +1,9 @@
-//
-//  ViewController.swift
-//  LeapYear
-//
-//  Created by VidyaShankari  on 08/05/19.
-//  Copyright © 2019 2019_SOW_DEV_007. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
 }
 
