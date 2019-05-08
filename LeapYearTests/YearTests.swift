44000c7d165a8e105ec613be2dbe10d4d580c37b
2019-SOW-DEV-007 <2019.SOW.DEV.007@gmail.com>
2019-05-09 01:56:24 +0200

    Code Refactored: Extracted code for initialising an year in a separate method.

--- LeapYearTests/YearTests.swift
+++ LeapYearTests/YearTests.swift
@@ -5,22 +5,26 @@ class YearTests: XCTestCase {
     
     func test_ReturnTrueForLeapYear_WhenAnYearIsDivisibleBy400() {
         let anYearDivisibleBy400 = 2000
-        let year = Year.init(anYearDivisibleBy400)
+        let year = createAnYear(anYearDivisibleBy400)
         
         XCTAssertTrue(year.isLeapYear())
     }
     
     func test_ReturnFalseForLeapYear_WhenAnYearIsNotDivisibleBy400() {
         let anYearNotDivisibleBy400 = 2001
-        let year = Year.init(anYearNotDivisibleBy400)
+        let year = createAnYear(anYearNotDivisibleBy400)
         
         XCTAssertFalse(year.isLeapYear())
     }
     
     func test_ReturnFalseForLeapYear_WhenAnYearIsDivisibleBy100ButNotBy400() {
         let anYearDivisibleBy100ButNotBy400 = 1700
-        let year = Year.init(anYearDivisibleBy100ButNotBy400)
+        let year = createAnYear(anYearDivisibleBy100ButNotBy400)
         
         XCTAssertFalse(year.isLeapYear())
+    }
+    
+    private func createAnYear(_ year: Int) -> Year {
+        return Year.init(year)
     }
 }
