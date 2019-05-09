d9a7a90f7dbb7c74ed383e9c00069ee000cc688f
2019-SOW-DEV-007 <2019.SOW.DEV.007@gmail.com>
2019-05-09 04:44:23 +0200

    Added text field delegates and button action methods

--- LeapYear/ViewController/LeapYearViewController.swift
+++ LeapYear/ViewController/LeapYearViewController.swift
@@ -1,14 +1,43 @@
 import UIKit
 
-class LeapYearViewController: UIViewController {
+class LeapYearViewController: UIViewController, UITextFieldDelegate, LeapYearView {
+    
+    
 
-    @IBOutlet weak var yearTextField: UITextField!
+    @IBOutlet weak private var yearTextField: UITextField!
+    @IBOutlet weak private var submitButton: UIButton!
+    
+    private var leapYearPresenter:LeapYearPresenter!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        leapYearPresenter = LeapYearPresenter.init(leapYearView: self)
     }
     
+    //MARK: Button Action
     @IBAction func submitButtonAction(_ sender: Any) {
+    }
+    
+    //Mark: Text Field Delegates
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        return leapYearPresenter.isValidNumber(number: string)
+    }
+    
+    //MARK: Leap Year View Protocols
+    func showYearLessThanGregorianCalendarYearMessage(message: String) {
+        
+    }
+    
+    func showLeapYearMessage(message: String) {
+        
+    }
+    
+    func showNonLeapYearMessage(message: String) {
+        
+    }
+    
+    func showInvalidYearMessage(message: String) {
+        
     }
 }
 
