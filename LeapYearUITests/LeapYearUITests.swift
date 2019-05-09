b675e2b1e6aaf9a238d6db39a7c6c9fda1767a6d
2019-SOW-DEV-007 <2019.SOW.DEV.007@gmail.com>
2019-05-09 07:37:24 +0200

    Added UITest to show alert with leap year message when user enters a valid leap year.

--- LeapYearUITests/LeapYearUITests.swift
+++ LeapYearUITests/LeapYearUITests.swift
@@ -2,8 +2,26 @@ import XCTest
 
 class LeapYearUITests: XCTestCase {
 
-    override func setUp() {
-        continueAfterFailure = false
+    func test_AlertIsShownWithLeapYearMessage_WhenUserEnterValidLeapYear() {
         XCUIApplication().launch()
+        
+        let app = XCUIApplication()
+        app.textFields["Enter an year"].tap()
+        
+        let key2 = app/*@START_MENU_TOKEN@*/.keys["2"]/*[[".keyboards.keys[\"2\"]",".keys[\"2\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        key2.tap()
+        
+        let key0 = app/*@START_MENU_TOKEN@*/.keys["0"]/*[[".keyboards.keys[\"0\"]",".keys[\"0\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        key0.tap()
+        
+        let key1 = app/*@START_MENU_TOKEN@*/.keys["1"]/*[[".keyboards.keys[\"1\"]",".keys[\"1\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        key1.tap()
+        
+        let key6 = app/*@START_MENU_TOKEN@*/.keys["6"]/*[[".keyboards.keys[\"6\"]",".keys[\"6\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
+        key6.tap()
+        
+        app.buttons["Submit"].tap()
+        app.alerts.buttons["OK"].tap()
+        
     }
 }
