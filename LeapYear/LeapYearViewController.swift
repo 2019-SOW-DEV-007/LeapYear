ca54b288ddfec3af53a245c12f116db8afdfda81
2019-SOW-DEV-007 <2019.SOW.DEV.007@gmail.com>
2019-05-09 03:22:11 +0200

    Renamed Class from ViewController to LeapYearViewController Updated storyboard with UI elements

---
+++ LeapYear/LeapYearViewController.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+class LeapYearViewController: UIViewController {
+
+    @IBOutlet weak var yearTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func submitButtonAction(_ sender: Any) {
+    }
+}
+
